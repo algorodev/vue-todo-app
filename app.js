09fed4d6836e756f602f1d0e135bae4860a24c13
Alejandro <me@algorodev.com>
2024-06-13 13:00:42 +0100

    feat: formatting code

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 Vue.createApp({
-    data() {
-        return {
-            todos: [],
-            inputValue: '',
-        }
-    },
-    methods: {
-        addTodo() {
-            this.todos.push(this.inputValue)
-            this.inputValue = ''
-        },
-        removeTodo(index) {
-            this.todos.splice(index, 1)
-        }
-    }
+	data() {
+		return {
+			todos: [],
+			inputValue: '',
+		}
+	},
+	methods: {
+		addTodo() {
+			this.todos.push(this.inputValue)
+			this.inputValue = ''
+		},
+		removeTodo(index) {
+			this.todos.splice(index, 1)
+		},
+	},
 }).mount('#todo-app')
